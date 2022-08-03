4de026d68c9f4ec3b33b97c44e5b82603946c951
NikDoe <nikd0e@yandex.ru>
2022-08-03 18:14:19 +0300

    feature(ts): parsing forms bodies

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -2,8 +2,26 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-	res.send(`<h1>Hello there</h1>`);
+router.get('/login', (req: Request, res: Response) => {
+	res.send(`
+		<form method="post">
+			<div>
+				<label>email</label>
+				<input name="email">
+			</div>
+			<div>
+				<label>password</label>
+				<input name="password" type="password">
+			</div>
+			<button>submit</button>
+		</form>
+	`);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+	const { email, password } = req.body;
+
+	res.send(email + password);
 });
 
 export { router };
