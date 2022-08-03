a749b89ca7afae1cc1e9bf334c5afa440c5370ec
NikDoe <nikd0e@yandex.ru>
2022-08-03 17:49:36 +0300

    init(ts): use basic routes w/ ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hi there');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port: number = 9000;
+
+app.get('/', (req: Request, res: Response) => {
+	res.send(`<h1>H1 there</h1>`);
+});
+
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
