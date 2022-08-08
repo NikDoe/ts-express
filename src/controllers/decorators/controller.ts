32c1dcafe7de2e5cd4993ec8e55de9451eff1831
NikDoe <nikd0e@yandex.ru>
2022-08-08 11:46:37 +0300

    refactor(ts): use metadata keys w/ enums

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
 import { Methods } from './Methods';
+import { MetadataKeys } from './MetadataKeys';
 
 export function controller(routePrefix: string) {
 	return function (target: Function) {
@@ -8,8 +9,12 @@ export function controller(routePrefix: string) {
 
 		for (let targetKey in target.prototype) {
 			const routeHandler = target.prototype[targetKey];
-			const path = Reflect.getMetadata('path', target.prototype, targetKey);
-			const method: Methods = Reflect.getMetadata('method', target.prototype, targetKey);
+			const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, targetKey);
+			const method: Methods = Reflect.getMetadata(
+				MetadataKeys.method,
+				target.prototype,
+				targetKey,
+			);
 
 			if (path) router[method](`${routePrefix}${path}`, routeHandler);
 		}
